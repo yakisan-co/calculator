4677a8f465a13f543a75d19f373b3a3f9b063269
yunusemreyakisan <yunusemreyakisan@outlook.com>
2023-12-08 15:40:34 +0300

    Added result animation

--- app/src/main/java/com/yakisan/calculator/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/yakisan/calculator/ui/screen/HomeScreen.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.os.Build
 import androidx.activity.compose.BackHandler
 import androidx.annotation.RequiresApi
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -21,6 +22,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.BottomCenter
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
@@ -90,7 +92,8 @@ fun HomeScreen(viewModel: CalculatorViewModel = hiltViewModel(), navController: 
                 //ALL PROCESS TEXT
                 Text(
                     modifier = Modifier
-                        .fillMaxWidth()
+                        .align(Alignment.End)
+                        .animateContentSize()
                         .padding(
                             start = MaterialTheme.dimens.medium1,
                             end = MaterialTheme.dimens.medium1,
@@ -105,7 +108,8 @@ fun HomeScreen(viewModel: CalculatorViewModel = hiltViewModel(), navController: 
                 //SUMMARY TEXT
                 Text(
                     modifier = Modifier
-                        .fillMaxWidth()
+                        .align(Alignment.End)
+                        .animateContentSize()
                         .padding(
                             start = MaterialTheme.dimens.medium1,
                             end = MaterialTheme.dimens.medium1,
