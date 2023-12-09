61a898dd9c9c4594b271f452bd030e42ef4ea277
yunusemreyakisan <yunusemreyakisan@outlook.com>
2023-12-09 16:06:19 +0300

    Updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.yakisan.calculator"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
